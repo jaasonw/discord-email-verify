69c1bfd30388ff5ceb88a08c480deae9a5427258
jaasonw <jayywong92@gmail.com>
2022-08-17 20:58:00 -0700

    test

--- pages/api/createUser.ts
+++ pages/api/createUser.ts
@@ -7,19 +7,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req.body);
-  const url = `https://discord.com/api/v10/interactions/${req.body["interactionId"]}/${req.body["interactionToken"]}/callback`;
+  const body = JSON.parse(req.body);
+  console.log(body);
+  const url = `https://discord.com/api/v10/interactions/${req.body["interactionId"]}/${req.body["interactionToken"]}/@original`;
   const prisma = new PrismaClient();
 
   await prisma.user.create({
     data: {
-      id: req.body["id"],
-      firstName: req.body["firstName"],
-      lastName: req.body["lastName"],
-      email: req.body["email"],
-      discordUser: req.body["discordUser"],
-      pronouns: req.body["pronouns"],
+      id: body["id"],
+      firstName: body["firstName"],
+      lastName: body["lastName"],
+      email: body["email"],
+      discordUser: body["discordUser"],
+      pronouns: body["pronouns"],
     },
   });
-  axios.patch(url, ephemeralMessageReply("Created entry in db"));
+  await axios.patch(url, ephemeralMessageReply("Created entry in db"));
+  res.status(200);
 }
