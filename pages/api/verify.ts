8e82694fbd7c46475cd7a3ba7fc77ec0c0aefa58
jaasonw <jayywong92@gmail.com>
2022-08-18 05:17:41 -0700

    trying discord.js again

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import axios, { AxiosError } from "axios";
+import { Client, GatewayIntentBits } from "discord.js";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -7,6 +8,8 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const prisma = new PrismaClient();
+  const discord = new Client({ intents: [GatewayIntentBits.Guilds] });
+  await discord.login(process.env.BOT_TOKEN);
   const code: string = (req.query["verificationCode"] as string) ?? "";
   try {
     const user = await prisma.user.findFirst({
@@ -22,19 +25,24 @@ export default async function handler(
         verificationCode: "verified",
       },
     });
-    const url = `https://discord.com/api/v10/guilds/${process.env.GUILD_ID}/members/${user}`;
-    await axios.patch(
-      url,
-      {
-        nick: `${user?.firstName} ${user?.lastName} (${user?.pronouns})`,
-        roles: [process.env.VERIFICATION_ROLE],
-      },
-      {
-        headers: {
-          Authorization: `Bot ${process.env.BOT_TOKEN}`,
-        },
-      }
-    );
+    discord.guilds.cache
+      .get(process.env.GUILD_ID ?? "")
+      ?.members.cache.get(user?.id ?? "")
+      ?.roles.add(process.env.VERIFICATION_ROLE ?? "");
+
+    // const url = `https://discord.com/api/v10/guilds/${process.env.GUILD_ID}/members/${user}`;
+    // await axios.patch(
+    //   url,
+    //   {
+    //     nick: `${user?.firstName} ${user?.lastName} (${user?.pronouns})`,
+    //     roles: [process.env.VERIFICATION_ROLE],
+    //   },
+    //   {
+    //     headers: {
+    //       Authorization: `Bot ${process.env.BOT_TOKEN}`,
+    //     },
+    //   }
+    // );
   } catch (error) {
     console.error(`Failed to verify UUID: ${req.query["verificationCode"]}`);
     console.error(`Error: ${JSON.stringify(error)}`);
