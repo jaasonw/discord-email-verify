b02f5a25da77427076eaab6612357d44db681bf7
jaasonw <jayywong92@gmail.com>
2022-08-19 14:43:32 -0700

    update verify page

--- pages/verify.tsx
+++ pages/verify.tsx
@@ -1,17 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 import { NextPage, NextPageContext } from "next";
+import Head from "next/head";
 
 const VerifyPage: NextPage = ({ user }: any) => {
   if (!user) {
     return (
       <h1>
-        Uh oh!, something went wrong, verify that your registration link is
-        correct and try again
+        Uh oh!, something went wrong. You may already be registered. Verify that
+        your registration link is correct or ask for help in register help if
+        the problem persists
       </h1>
     );
   }
   return (
     <>
+      <Head>
+        <title>Verify your email</title>
+      </Head>
       <h1>Hi {user.firstName}! Welcome to ACM at CSUF</h1>
       <div>
         Click{" "}
