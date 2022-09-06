0bf5b581c673de1d145fe07e58ad47b593302091
Charlie Taylor <56566188+Peekoe@users.noreply.github.com>
2022-09-06 00:23:03 -0700

    Add /configure for Register button (#9)
    * button configuration time
    
    * curly brace mayhem
    
    * refactor, add some logs for debug
    
    * lmao
    
    * debug sending message
    
    * SPECIFY THE TYPE <T> FUCK
    
    * please 😿😭🔮
    
    * pwease uwu ♥🙉😻
    
    * clean up logs, awesome

--- pages/api/interactions.ts
+++ pages/api/interactions.ts
@@ -3,6 +3,7 @@ import {
   InteractionType,
   verifyKey,
 } from 'discord-interactions';
+import { ButtonBuilder } from 'discord.js';
 import { NextRequest } from 'next/server';
 import { ephemeralMessageReply, loadingMessage } from '../../lib/message';
 import { ModalBuilder, TextInputBuilder } from '../../lib/modal';
@@ -12,114 +13,133 @@ const REGISTER_COMMAND = {
   description: 'Get a registration link to register for the club',
 };
 
-export default async function handler(req: NextRequest) {
-  if (req.method === 'POST') {
-    const signature = req.headers.get('x-signature-ed25519') ?? '';
-    const timestamp = req.headers.get('x-signature-timestamp') ?? '';
-    const body = await req.clone().arrayBuffer();
-    console.log(body);
-    const isValidRequest = verifyKey(
-      body,
-      signature,
-      timestamp,
-      process.env.DISCORD_TOKEN ?? ''
-    );
-    if (!isValidRequest) {
-      console.error('Bad request signature');
-      return new Response('Bad request signature', { status: 401 });
-    }
-    const message = JSON.parse(await req.text());
-    console.log(message);
-    // ACK pings from Discord
-    if (message.type === InteractionType.PING) {
-      console.log('Handling Ping request');
-      return send(200, {
-        type: InteractionResponseType.PONG,
-      });
-    } else if (message.type == InteractionType.APPLICATION_COMMAND) {
-      switch (message.data.name.toLowerCase()) {
-        case REGISTER_COMMAND.name.toLowerCase():
-          console.log('Handling register command');
-          return send(
-            200,
-            new ModalBuilder()
-              .setCustomId('registration_modal')
-              .setTitle('Register for access to the server!')
-              .addComponent(
-                new TextInputBuilder()
-                  .setCustomId('first_name')
-                  .setLabel('First Name')
-                  .setMinLength(2)
-                  .setMaxLength(32)
-                  .setPlaceholder('frank')
-                  .setRequired(true)
-              )
-              .addComponent(
-                new TextInputBuilder()
-                  .setCustomId('last_name')
-                  .setLabel('Last Name')
-                  .setMinLength(2)
-                  .setMaxLength(32)
-                  .setPlaceholder('Bot')
-                  .setRequired(true)
-              )
-              .addComponent(
-                new TextInputBuilder()
-                  .setCustomId('email')
-                  .setLabel('CSUF Email')
-                  .setMinLength(2)
-                  .setMaxLength(320)
-                  .setPlaceholder('frankBot@csu.fullerton.edu')
-                  .setRequired(true)
-              )
-              .addComponent(
-                new TextInputBuilder()
-                  .setCustomId('pronouns')
-                  .setLabel('Preferred Pronouns (Optional)')
-                  .setMinLength(2)
-                  .setPlaceholder('He/Him, She/Her, They/Them, etc')
-                  .setRequired(false)
-              )
-          );
-      }
-    } else if (message.type == InteractionType.APPLICATION_MODAL_SUBMIT) {
-      // Handle modal responses
-      console.info(message);
-      console.info(message.data.components[0]);
-      switch (message.data.custom_id) {
-        case 'registration_modal':
-          const url = `${process.env.DEPLOYMENT_URL}/api/createUser`;
-          const firstName = message.data.components[0].components[0].value;
-          const lastName = message.data.components[1].components[0].value;
-          const email = message.data.components[2].components[0].value;
-          const pronouns = message.data.components[3].components[0].value;
-          console.info(`First Name: ${firstName}`);
-          console.info(`Last Name: ${lastName}`);
-          console.info(`Email: ${email}`);
-          console.info(`Pronouns: ${pronouns}`);
-          console.info(`URL: ${url}`);
+const CONFIGURE_COMMAND = {
+  name: 'configure',
+  description: 'Set bot channel',
+};
 
-          // const validEmail = EmailValidator.validate(email);
-          fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({
-              botToken: process.env.DISCORD_TOKEN,
-              interactionId: message.id,
-              interactionToken: message.token,
-              id: message.member.user.id,
-              firstName: firstName,
-              lastName: lastName,
-              email: email,
-              discordUser: `${message.member.user.username}#${message.member.user.discriminator}`,
-              pronouns: pronouns,
-            }),
-          });
-          return send(200, loadingMessage());
-      }
-    }
-  } else {
+export default async function handler(req: NextRequest) {
+  if (req.method !== 'POST') {
     return send(405, 'Method not allowed');
   }
+  
+  const signature = req.headers.get('x-signature-ed25519') ?? '';
+  const timestamp = req.headers.get('x-signature-timestamp') ?? '';
+  const body = await req.clone().arrayBuffer();
+  console.log(body);
+  const isValidRequest = verifyKey(
+    body,
+    signature,
+    timestamp,
+    process.env.DISCORD_TOKEN ?? ''
+  );
+  if (!isValidRequest) {
+    console.error('Bad request signature');
+    return new Response('Bad request signature', { status: 401 });
+  }
+  const message = JSON.parse(await req.text());
+  console.log(message);
+  // ACK pings from Discord
+  if (message.type === InteractionType.PING) {
+    console.log('Handling Ping request');
+    return send(200, {
+      type: InteractionResponseType.PONG,
+    });
+  }
+  
+  if (message.type == InteractionType.MESSAGE_COMPONENT) {
+    console.log('Handling register command');
+        return send(
+          200,
+          new ModalBuilder()
+            .setCustomId('registration_modal')
+            .setTitle('Register for access to the server!')
+            .addComponent(
+              new TextInputBuilder()
+                .setCustomId('first_name')
+                .setLabel('First Name')
+                .setMinLength(2)
+                .setMaxLength(32)
+                .setPlaceholder('frank')
+                .setRequired(true)
+            )
+            .addComponent(
+              new TextInputBuilder()
+                .setCustomId('last_name')
+                .setLabel('Last Name')
+                .setMinLength(2)
+                .setMaxLength(32)
+                .setPlaceholder('Bot')
+                .setRequired(true)
+            )
+            .addComponent(
+              new TextInputBuilder()
+                .setCustomId('email')
+                .setLabel('CSUF Email')
+                .setMinLength(2)
+                .setMaxLength(320)
+                .setPlaceholder('frankBot@csu.fullerton.edu')
+                .setRequired(true)
+            )
+            .addComponent(
+              new TextInputBuilder()
+                .setCustomId('pronouns')
+                .setLabel('Preferred Pronouns')
+                .setMinLength(2)
+                .setPlaceholder('He/Him, She/Her, They/Them, etc')
+                .setRequired(false)
+            )
+        );
+  }
+
+  if (message.type == InteractionType.APPLICATION_COMMAND) {
+    const url = `${process.env.DEPLOYMENT_URL}/api/sendButton`;
+    const channelId = message.data.options[0].value;
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        channelId: channelId
+      }),
+    });
+
+    return send(200, loadingMessage());
+  }
+
+  if (message.type == InteractionType.APPLICATION_MODAL_SUBMIT) {
+    // Handle modal responses
+    console.info(message);
+    console.info(message.data.components[0]);
+    const url = `${process.env.DEPLOYMENT_URL}/api/createUser`;
+    const firstName = message.data.components[0].components[0].value;
+    const lastName = message.data.components[1].components[0].value;
+    const email = message.data.components[2].components[0].value;
+    const pronouns = message.data.components[3].components[0].value;
+    console.info(`First Name: ${firstName}`);
+    console.info(`Last Name: ${lastName}`);
+    console.info(`Email: ${email}`);
+    console.info(`Pronouns: ${pronouns}`);
+    console.info(`URL: ${url}`);
+
+    // const validEmail = EmailValidator.validate(email);
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        botToken: process.env.DISCORD_TOKEN,
+        interactionId: message.id,
+        interactionToken: message.token,
+        id: message.member.user.id,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        discordUser: `${message.member.user.username}#${message.member.user.discriminator}`,
+        pronouns: pronouns,
+      }),
+    });
+    return send(200, loadingMessage());
+  }
+
+  return send(400, 'Invalid request');
 }
 
 function send(statusCode: number, body: Object) {
---
+++ pages/api/sendButton.ts
@@ -0,0 +1,28 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, GatewayIntentBits, TextChannel } from "discord.js";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function name(req: NextApiRequest, res: NextApiResponse) {
+  const body = JSON.parse(req.body);
+
+  const channelId = body['channelId'].trim();
+
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+  await client.login(process.env.BOT_TOKEN);
+
+  console.debug('Creating button');
+
+  const button = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(
+				new ButtonBuilder()
+          .setCustomId('registration_modal')
+          .setLabel('Register')
+          .setEmoji('✅')
+          .setStyle(ButtonStyle.Primary),
+    );
+  
+  await client.guilds.fetch();
+  const channel = client.channels.cache.get(channelId) as TextChannel;
+  let message = await channel.send({ content: 'Click here!', components: [button] });
+
+  res.status(200).json({ status: message });
+}
