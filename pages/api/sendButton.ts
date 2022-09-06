e56f3841834bf1c8672df9af950f66c59e4f4b3a
jason wong <jayywong92@gmail.com>
2022-09-06 00:53:13 -0700

    add button message, readd optional (#13)

--- pages/api/sendButton.ts
+++ pages/api/sendButton.ts
@@ -5,6 +5,7 @@ export default async function name(req: NextApiRequest, res: NextApiResponse) {
   const body = JSON.parse(req.body);
 
   const channelId = body['channelId'].trim();
+  const buttonMessage = body['message']?.trim() ?? "";
 
   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
   await client.login(process.env.BOT_TOKEN);
@@ -22,7 +23,10 @@ export default async function name(req: NextApiRequest, res: NextApiResponse) {
   
   await client.guilds.fetch();
   const channel = client.channels.cache.get(channelId) as TextChannel;
-  let message = await channel.send({ content: 'Click here!', components: [button] });
+  let message = await channel.send({
+    content: buttonMessage,
+    components: [button],
+  });
 
   res.status(200).json({ status: message });
 }
